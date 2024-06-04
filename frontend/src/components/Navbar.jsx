845ffada2e3cb69bf58ffe1d4723b11eb0b82a37
Matae300 <150396856+Matae300@users.noreply.github.com>
2024-06-03 20:29:07 -0400

    added navbar, added transcation model, added more files

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import '../assets/Navbar.css'
+
+const Navbar = () => {
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+        ⚔️ Guardian Bank
+        </Link>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-links">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-links">
+              Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/account" className="nav-links">
+              Account
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+
+  return (
+      <div className="login-container">
+        <h1>Guardian Bank</h1>
+      </div>
+  );
+};
+
+export default Home;
